7447d4dc10ae6be88e8ae8619d1a1d3c85ce6b52
Francisco Leudes Bezerra Neto <108896134+Leudes@users.noreply.github.com>
2026-06-22 17:12:51 -0300

    Update latitude and longitude for AR points

--- app.js
+++ app.js
@@ -8,8 +8,8 @@ const AR_POINTS = [
   {
     id: "ra-releitura-1",
     image: "assets/releitura-1.svg",
-    latitude: -4.979991076004118,
-    longitude: -39.05593834243967,
+    latitude: -4.969682,
+    longitude: -39.013989,
     width: 4,
     height: 2.6,
     scale: "10 10 10"
@@ -17,8 +17,8 @@ const AR_POINTS = [
   {
     id: "ra-releitura-2",
     image: "assets/releitura-2.svg",
-    latitude: -4.979891076004118,
-    longitude: -39.05583834243967,
+    latitude: -4.969677,
+    longitude: -39.013989,
     width: 4,
     height: 2.6,
     scale: "10 10 10"
@@ -26,8 +26,8 @@ const AR_POINTS = [
   {
     id: "ra-releitura-3",
     image: "assets/releitura-3.svg",
-    latitude: -4.980091076004118,
-    longitude: -39.05603834243967,
+    latitude: -4.969677,
+    longitude: -39.013989,
     width: 4,
     height: 2.6,
     scale: "10 10 10"
